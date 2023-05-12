59687b1443d92668bc79cbedfd52af1a9fc43bd8
Ankur srivastava <ankursrivastava2070@gmail.com>
2023-05-12 22:00:28 +0530

    mongo uri updated

--- server/db.js
+++ server/db.js
@@ -6,7 +6,7 @@ module.exports = () => {
 		useUnifiedTopology: true,
 	};
 	try {
-		mongoose.connect("mongodb://localhost:27017/myRandomSite", connectionParams);
+		mongoose.connect(process.env.DB, connectionParams);
 		console.log("Connected to database successfully");
 	} catch (error) {
 		console.log(error);
